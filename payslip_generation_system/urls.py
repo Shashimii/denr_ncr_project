fca78e9a5502794c4b661b55bcf503c5e85e93c4
Shashimii <134041347+Shashimii@users.noreply.github.com>
2025-07-23 12:31:22 +0800

    * categorized the routes

--- payslip_generation_system/urls.py
+++ payslip_generation_system/urls.py
@@ -6,32 +6,36 @@ from django.conf import settings
 from django.conf.urls.static import static
 
 urlpatterns = [
+    # misc
     path('', views.index, name='index'),
-    
     path('test/', views.test, name='test'),
     
-    # login page
+    # login
     path('login/', views.login, name='login'),
-    path('dashboard/', views.dashboard, name='dashboard'),
     
+    # Dashboard
+    path('dashboard/', views.dashboard, name='dashboard'),
+
+    # Employee
+    path('employee/view/<int:emp_id>/', views.view_employee, name='view_employee'),
     path('add_employee_profile/', views.add_employee, name='add_employee_profile'),
     path('add-employee/', views.add_employee, name='add_employee'),
     path('employee/edit/<int:emp_id>/', views.edit_employee, name='edit_employee'),
     path('employee/delete/<int:emp_id>/', views.delete_employee, name='delete_employee'),
     path('attachments/delete/<int:attachment_id>/', views.delete_attachment, name='delete_attachment'),
-    path('employee/view/<int:emp_id>/', views.view_employee, name='view_employee'),
+
+    # Adjustment
     path('employee/adjustments/<int:emp_id>/', views.adjustments_employee, name='adjustments_employee'),
     path('employee/adjustments/add/<int:emp_id>/', views.add_adjustment, name='add_adjustment'),
 
+    # Payslip
+    path('payslip/', views.payslip, name='payslip'),
 
-    # get data from database
+    # Data Routes
     path('employee-data/', views.employee_data_json, name='employee_data_json'),
     path('employee/adjustments/json/<int:emp_id>/', views.employee_adjustments_json, name='employee_adjustments_json'),
-    
-    
-    path('payslip/', views.payslip, name='payslip'),
 
 ]
 
 if settings.DEBUG:
-    urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+    urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)  
